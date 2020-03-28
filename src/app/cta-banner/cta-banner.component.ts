767401714912e159178c4fba18771aadfbfb7e89
Erin Denae <erindenae@erins-mbp.lan>
2020-03-28 00:53:44 -0500

    add cta-banner component

---
+++ src/app/cta-banner/cta-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'short-cta-banner',
+  templateUrl: './cta-banner.component.html',
+  styleUrls: ['./cta-banner.component.scss']
+})
+export class CtaBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
